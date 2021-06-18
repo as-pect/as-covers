f239a04300d88c4b0e4a4cc518421acac97826f0
JairusSW <jairus.v.tanaka@outlook.com>
2021-06-18 11:20:53 -0700

    Testing

--- tests/test.js
+++ tests/test.js
@@ -6,6 +6,7 @@ const loader = require("@assemblyscript/loader");
 const fs = require("fs");
 const { Covers } = require("../packages/glue/lib/index");
 const covers = new Covers();
+const Linecol = require('line-column')
 
 const wasmModule = loader.instantiateSync(
   fs.readFileSync("./output/output.wasm"),
@@ -18,6 +19,17 @@ wasmModule.exports._start();
 const JSONreport = JSON.stringify(covers.toJSON(), null, 2)
 
 const resultSnapShot = fs.readFileSync('./output/coverReportSnapshot.json').toString()
+
+const linecol = Linecol(resultSnapShot)
+
+for (let i = 0; i < resultSnapShot.length; i++) {
+
+  if (JSONreport[i] !== resultSnapShot[i]) {
+    const lc = linecol.fromIndex(i+1)
+    throw new Error(`Result Failed at ./output/coverReport.json:${lc.line}:${lc.col}`)
+  }
+
+}
 
 if (resultSnapShot === JSONreport) {
     console.log('Test Result: Passed.')
